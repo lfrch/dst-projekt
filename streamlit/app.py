b25d1fcbff34d31d94aa83e723228aacf2cae3fe
lfrch <lf073@hdm-stuttgart.de>
2024-01-18 22:31:12 +0100

    Dashboard aufbauen

--- streamlit/app.py
+++ streamlit/app.py
@@ -30,6 +30,8 @@ st.write("Präsentiert von Lea Frech")
 # -------------------#
 # SIDEBAR
 
+st.sidebar.title("Übersicht zu relevanten Fakten rund um die Arbeitsproduktivität")
+
 
 # -------------------#
 # BODY
@@ -40,50 +42,138 @@ colors = alt.Scale(
 )
 
 st.markdown("## Die Arbeitsproduktivität unseres Unternehmens im Vergleich zu anderen Ländern") 
-st.markdown("Betrachtet werden interne Daten des Jahres **2022** und reicht zurück bis in das Jahr **2019**.") 
+st.markdown('Betrachtet werden interne Daten des Jahres :orange[**2022**] und reicht zurück bis in das Jahr :blue[**2019**].')
 
 # Map Ansicht, um zu zeigen, woher die Daten kommen -> Neuen df erzeugen mit Längen & Breitengrade für GBR und DEU
 st.markdown("#### Geographische Übersicht: Karte")
 
 
-# Balkendiagramm (paar Fakten ergänzen!) -> Filter pro Jahr, als interaktivität
+# *********************Balkendiagramm *****************************
+#paar Fakten ergänzen! -> Filter pro Jahr, als interaktivität
+
 st.markdown("#### Gearbeitete Stunden gemessen an einer normalen 5-Tage Woche à 40h")
 
-barchart = alt.Chart(df_selectedlocations).mark_bar().encode(
-        x=alt.X('LOCATION').axis(
-            title='Location',
-            titleAnchor='start',
-            labelAngle=0,
-            grid=False,
-            labelColor='black',
-            tickColor='black'),
-        y=alt.Y('VALUE').axis(
-            title='Value',
-            titleAnchor='end',
-            grid=False,
-            labelColor='black',
-            tickColor='black'),
-        color=alt.Color('TIME', scale=colors, legend=None),
-        tooltip=['TIME']
-    ).properties(
-        width=600,
-        height=400
+#Code für Balkendiagramm
+barchart1 = alt.Chart(df_selectedlocations).mark_bar().encode(
+    x=alt.X('VALUE', scale=alt.Scale(bins=[0,1000,2000,3000,4000,5000,6000,7000])).axis(
+        title='Anzahl Stunden',
+        titleAnchor='start',
+        grid=False,
+        labelColor='black',
+        tickColor='grey'),
+    y=alt.Y('LOCATION').axis(
+        title='Standort',
+        titleAnchor='middle',
+        titleAngle=-90,
+        labelAngle= 0,
+        grid=False,
+        labelColor='black',
+        tickColor='black'),
+    color=alt.Color('TIME:O', scale=colors),
+    #column=alt.Column('day:N', sort=['Thur','Fri','Sat','Sun'], header=alt.Header(orient='bottom'))  
+).properties(
+    width=800,
+    height=500,
+    title=alt.Title(
+        "Hours worked",
     )
+)
 
-st.altair_chart(barchart, use_container_width=True )
+barchart1_labels = alt.Chart(df_selectedlocations).mark_text(baseline='middle', color='black').encode(
+    x=alt.X('VALUE'),
+    y=alt.Y('LOCATION')  
+)
+
+barchart1_final = alt.layer(barchart1,barchart1_labels).configure_view(
+    strokeWidth=0
+).configure_title(
+    fontSize=25,
+    anchor='middle',
+    fontWeight='bold',
+).configure_axis(
+    labelFontSize = 11,
+    titleFontSize = 12,
+    titleFontWeight= 'normal',
+    titleColor='grey'
+).configure_text(
+    fontWeight='bold',
+    fontSize = 12
+)
+
+barchart1_final
+
+st.write('Hier Filter einbauen?')
+st.markdown("""---""")
 
 
-# Boxplot: Hours worked to exit poverty, im Verlauf der Jahre 2019-22
+# ***********Boxplot: Hours worked to exit poverty, im Verlauf der Jahre 2019-22*******************
+
 st.markdown("#### Boxplot: Benötigte Arbeitsstunden, um nicht in Armut zu leben innerhalb der letzten Jahre")
+st.markdown("""---""")
 
-# Häufigkeitsverteilung:
+df2_selectedlocations = pd.read_csv("/Users/Lea/Desktop/dst-projekt/df2_selectedlocations.csv")
+
+colors2 = alt.Scale(
+    range=['#58508d','#ff6361','#ffa600']
+)
+
+#Code Boxplot
+boxplot = alt.Chart(df2_selectedlocations).mark_boxplot(size=80, extent=0.5).encode(
+    x=alt.X('SUBJECT').axis(
+        title=None,
+        labelAngle=0,
+        grid=False,
+        labelColor='black',
+        tickColor='grey'),
+    y=alt.Y('VALUE:Q', scale=alt.Scale(bins=[0,2,4,6,8,10,12,14,16,18],domain=(0,20))).axis(
+        titleAnchor='start',
+        grid=False,
+        labelColor='black',
+        tickColor='black'),
+    color=alt.Color('SUBJECT', scale=colors2, legend=None),
+).properties(
+    width=800,
+    height=500,
+    title=alt.Title(
+        "Hours needed to exit poverty",
+        subtitle="in relation to family status at average wage",
+    )
+)
+
+boxplot_final = alt.layer(boxplot).configure_view(
+    strokeWidth=0
+).configure_title(
+    fontSize=25,
+    anchor='middle',
+    fontWeight='bold',
+).configure_axis(
+    labelFontSize = 11,
+    labelFontWeight='bold',
+    titleFontSize = 12,
+    titleFontWeight= 'normal',
+    titleColor='grey'
+).configure_text(
+    fontWeight='bold',
+    fontSize = 12
+)
+
+boxplot_final
+
+st.markdown("""---""")
+
+# *********** Histplot *******************
+
 st.markdown("#### Histogramm")
+st.markdown("""---""")
 
-# Scatterplot
+
+# *********** Scatterplot *******************
 st.markdown("#### Scatterplot")
+st.write("Hier Zusammenhang der gearbeiteten Stunden Wöchentlich und Jährlich zeigen")
+st.markdown("""---""")
 
 
-# Linechart: Productivity Forecast
+# ***********Linechart: Productivity Forecast*******************
 
 df3_selectedlocations = pd.read_csv("/Users/Lea/Desktop/dst-projekt/df3_selectedlocations.csv")
 europe = pd.read_csv("/Users/Lea/Desktop/dst-projekt/europe_forecast.csv")
@@ -95,17 +185,17 @@ colors_linechart3 = alt.Scale(
     range=['#ffa600','#ff6361',]
 )
 
-# *************************************************************
-
 linechart3 = alt.Chart(df3_selectedlocations).mark_line().encode(
     x=alt.X('TIME:O', title='Jahr').axis(
         titleAnchor='start',
         labelAngle= -0,
+        labelColor='black',
         ),
     y=alt.Y('VALUE').scale(domain=(0.85,1.15)).axis(
         title='GDP per hour worked',
         titleAnchor='end',
         grid= False,
+        labelColor='black',
         ),
     strokeWidth=alt.value(3), 
     color=alt.Color('LOCATION', scale=colors_linechart3),
@@ -155,7 +245,7 @@ europe_line_labels = alt.Chart(europe).mark_text(align='left', dx=3).encode(
 linechart3_final = alt.layer(linechart3, linechart3_labels, europe_linechart, europe_line_labels).configure_view(
     strokeWidth=0
 ).configure_title(
-    fontSize=20,
+    fontSize=25,
     anchor='middle',
     fontWeight='bold',
 ).configure_axis(
@@ -169,6 +259,8 @@ linechart3_final = alt.layer(linechart3, linechart3_labels, europe_linechart, eu
 )
 
 linechart3_final
+
+st.markdown("""---""")
 
 ### -------------------###
 # END OF APP
